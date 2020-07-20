615871ab12979d5733d62311c0982ff37b9ad087
Black-x98 <tariq.hasan.csedu21@gmail.com>
2020-07-20 14:06:01 +0600

    commiting text-input version of the program

--- TalkingBot.py
+++ TalkingBot.py
@@ -1,89 +1,111 @@
 import pyttsx3 as tts
-import speech_recognition as sr
+import datetime
 
 engine = tts.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[2].id)
 g_txt = "Hi, this is your talking Py-Bot."
 print(g_txt)
-r = sr.Recognizer()
+
 
 tts.speak(g_txt)
 bot_name = "blank"
-with sr.Microphone() as source:
 
-    str = "\nSay something: "
-    r.adjust_for_ambient_noise(source, 3)
-    print(str)
+str = "\nSay something: "
+print(str)
 
-    speech = ''
-    while(True):
-        try:
-            tts.speak("Say something")
-            audio = r.listen(source)
-            speech = r.recognize_google(audio,language='en-US')
-            if "hi" in speech or "hello" in speech:
-                str = "\n*You*: " + speech
-                print(str)
-                str = "Hello Tariq."
-                tts.speak(str)
-                print("*Py-Bot*: " + str)
+speech = ''
+while(True):
 
-            elif "bye" in speech or "turn off" in speech:
-                str = "\n*You*: " + speech
-                print(str)
-                str = "Okay Tariq, I am shutting down, bye."
-                tts.speak(str)
-                print("*Py-Bot*: " + str)
-                break
+    tts.speak("Say something")
+    speech = input()
+    if "hi" in speech or "hello" in speech:
+        str = "\n*You*: " + speech
+        print(str)
+        str = "Hello Tariq."
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
 
-            elif "set your name" in speech:
-                str = "\n*You*: " + speech
-                bot_name = str.split()[-1]
-                print(str)
-                str = "Okay Tariq, from now on my name is " + bot_name
-                tts.speak(str)
-                print("*Py-Bot*: " + str)
+    elif "bye" in speech or "turn off" in speech:
+        str = "\n*You*: " + speech
+        print(str)
+        str = "Okay Tariq, I am shutting down, bye."
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
+        break
 
-            elif "your name" in speech:
-                if bot_name=="blank":
-                    str = "\n*You*: " + speech
-                    print(str)
-                    str = "I haven't been given any personal name yet.\nBut you can call me Py-Bot for now."
-                    tts.speak(str)
-                    print("*Py-Bot*: " + str)
-                else:
-                    str = "\n*You*: " + speech
-                    print(str)
-                    str = "My name is " + bot_name
-                    tts.speak(str)
-                    print("*Py-Bot*: " + str)
+    elif "no" in speech: # "what" in speech or "tell" in speech and
+        str = "\n*You*: " + speech
+        bot_name = str.split()[-1]
+        print(str)
+        str = "Today is " + datetime.datetime.now().__str__()
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
 
-            elif "language" in speech:
-                str = "\n*You*: " + speech
-                print(str)
-                str = "I am coded using Python."
-                tts.speak(str)
-                print("*Py-Bot*: " + str)
+    elif "today" in speech: # "what" in speech or "tell" in speech and
+        str = "\n*You*: " + speech
+        bot_name = str.split()[-1]
+        print(str)
+        datenow = datetime.datetime.now()
+        str = "Today is " + datenow.strftime("%A") + " " + datenow.strftime("%B") + " " +  datenow.strftime("%C") + " year " + (datenow.year).__str__()
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
 
-            elif "how are you" in speech:
-                str = "\n*You*: " + speech
-                print(str)
-                str = "I am just a chatbot, that doesn't apply for me.\nBut I hope you are doing well."
-                tts.speak(str)
-                print("*Py-Bot*: " + str)
-            else:
-                str = "\nYou said: " + speech
-                tts.speak("You said: " + speech)
-                print(str)
+    elif "change" in speech or "set" in speech and "your name" in speech:
+        str = "\n*You*: " + speech
+        bot_name = str.split()[-1]
+        print(str)
+        str = "Okay Tariq, from now on my name is " + bot_name
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
 
-        except sr.UnknownValueError:
-            str = "\nSorry, could not understand what you said."
-            tts.speak(str)
+    elif "change" in speech and "your name" in speech or "set" in speech and "your name" in speech:
+        str = "\n*You*: " + speech
+        bot_name = str.split()[-1]
+        print(str)
+        str = "Okay Tariq, from now on my name is " + bot_name
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
+
+    elif "what" in speech or "tell" in speech and "your name" in speech:
+        if bot_name=="blank":
+            str = "\n*You*: " + speech
             print(str)
-
-        except sr.RequestError as e:
-            str = "\nRequest Error"
+            str = "I haven't been given any personal name yet.\nBut you can call me Py-Bot for now."
             tts.speak(str)
+            print("*Py-Bot*: " + str)
+        else:
+            str = "\n*You*: " + speech
             print(str)
+            str = "My name is " + bot_name
+            tts.speak(str)
+            print("*Py-Bot*: " + str)
+
+    elif "laugh" in speech:
+        str = "\n*You*: " + speech
+        print(str)
+        str = "Ha ha ha ha ha ha!!!!!"
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
+
+
+    elif "language" in speech:
+        str = "\n*You*: " + speech
+        print(str)
+        str = "I am coded using Python."
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
+
+    elif "how are you" in speech:
+        str = "\n*You*: " + speech
+        print(str)
+        str = "I am just a chatbot, that doesn't apply for me.\nBut I hope you are doing well."
+        tts.speak(str)
+        print("*Py-Bot*: " + str)
+    else:
+        str = "\nYou said: " + speech
+        tts.speak("You said: " + speech)
+        print(str)
+
+
 
